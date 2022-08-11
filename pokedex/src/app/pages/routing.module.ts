10de56365486aa8c245005ed2f8ddef22b1f93fa
TaciAlves <95450101+TaciAlves@users.noreply.github.com>
2022-08-11 06:00:32 -0300

    Update routing.module.ts

--- pokedex/src/app/pages/routing.module.ts
+++ pokedex/src/app/pages/routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailsComponent } from './details/details.component'; 
+import { DetailsComponent } from '././details/details.component'; 
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
